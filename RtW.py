afab8a162d6efb02d9f8d5d97b5c371e6b10fe0f
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2023-04-30 17:12:14 -0700

    Almost done

--- RtW.py
+++ RtW.py
@@ -3,7 +3,7 @@ import gettext
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
-from gi.repository import Gtk, Adw, Gio
+from gi.repository import Gtk, Adw, Gio, Pango, Gdk
 
 # For code optimization by avoiding duplicate settings classes
 # Adds org.gnome.shell because the extension classes need it and
@@ -11,6 +11,23 @@ from gi.repository import Gtk, Adw, Gio
 known_schemas = {}
 _ = gettext.gettext
 
+ACCENT_BUTTON_CSS = b"""
+.accent-button {
+  border-radius: 9999px;
+  padding: 3px;
+  background: transparent;
+  min-width: 24px;
+  min-height: 24px;
+}
+
+.accent-button:checked {
+  box-shadow: 0 0 0 3px @accent_bg_color;
+}
+
+.accent-button > widget {
+  border-radius: 9999px;
+}
+"""
 
 class SettingObject:
     def __init__(self, schema: str, key: str):
@@ -23,10 +40,10 @@ class SettingObject:
         raise NotImplementedError("on_setting_changed not implemented by subclass")
 
 
-class ActionRow(Adw.ActionRow, SettingObject):
+class ActionRow(SettingObject, Adw.ActionRow):
     def __init__(self, title: str, subtitle: str, schema: str, key: str):
-        Adw.ActionRow.__init__(self, title=_(title), subtitle=_(subtitle))
         SettingObject.__init__(self, schema, key)
+        Adw.ActionRow.__init__(self, title=_(title), subtitle=_(subtitle))
         self.set_activatable(True)
         self.set_selectable(False)
 
@@ -39,17 +56,19 @@ class ActionRow(Adw.ActionRow, SettingObject):
 
 
 class SwitchRow(ActionRow):
-    def __init__(self, title: str, subtitle: str, schema: str, key: str):
+    def __init__(self, title: str, subtitle: str | None, schema: str, key: str):
         super().__init__(title, subtitle, schema, key)
         self.switch = Gtk.Switch()
+        self.switch.set_valign(Gtk.Align.CENTER)
         self.switch.connect("state-set", self.on_switch_set)
         self.set_main_widget(self.switch)
 
     def on_setting_changed(self, *args):
-        self.set_active(self.settings.get_boolean(self.key))
+        self.switch.set_state(self.settings.get_boolean(self.key))
 
     def on_switch_set(self, switch, state):
-        self.settings.set_boolean(self.key, state)
+        if state != self.settings.get_boolean(self.key):
+            self.settings.set_boolean(self.key, state)
 
 
 class DropdownItems:
@@ -60,6 +79,14 @@ class DropdownItems:
         self.translated_items = [_(item) for item in user_items]
         self.backend_items = backend_items
         self.item_descriptions = item_descriptions
+        self.amount = len(user_items)
+
+        if self.amount != len(backend_items):
+            raise ValueError("The amount of user items and backend items must be the same!")
+        if self.amount != len(item_descriptions) and item_descriptions is not None and len(item_descriptions) != 0:
+            raise ValueError("The amount of user items and item descriptions must be the same!")
+        if self.amount == 0:
+            raise ValueError("The amount of user items must be at least 1!")
 
     @classmethod
     def new_same_items(cls, items: list, item_descriptions=None):
@@ -71,39 +98,67 @@ class DropdownItems:
     @classmethod
     def new_lowercase_items(cls, items: list, item_descriptions=None):
         if item_descriptions is None:
+            print(items, [item.lower() for item in items])
             return cls(items, [item.lower() for item in items])
         else:
             return cls(items, [item.lower() for item in items], item_descriptions)
 
 
 class DropdownRow(ActionRow):
-    def __init__(self, title: str, subtitle: str, schema: str, key: str, items: DropdownItems):
+    def __init__(self, title: str, subtitle: str | None, schema: str, key: str, items: DropdownItems):
         super().__init__(title, subtitle, schema, key)
-        self.add_suffix(self.dropdown)
         self.items = items
+        self.error = False
 
         self.dropdown = Gtk.DropDown.new_from_strings(self.items.translated_items)
-        self.dropdown.connect("changed", self.on_dropdown_changed)
+        self.errorless_model = self.dropdown.get_model()
+        self.dropdown.set_vexpand(False)
+        self.dropdown.set_valign(Gtk.Align.CENTER)
         self.set_main_widget(self.dropdown)
+        self.select_item()
+        self.dropdown.connect("notify::selected", self.on_dropdown_changed)
 
     def on_setting_changed(self, *args):
-        try:
-            new_id = self.items.backend_items.index(self.settings.get_string(self.key))
-            if new_id != self.dropdown.get_selected():
-                self.dropdown.set_selected(new_id)
-            self.remove_css_class("error")
-        except ValueError:
-            self.dropdown.set_selected(-1)
+        self.select_item()
+
+    def set_error(self, error: bool):
+        self.error = error
+        if error:
             self.add_css_class("error")
+        else:
+            self.remove_css_class("error")
+            self.dropdown.set_model(self.errorless_model)
 
-    def on_dropdown_changed(self, dropdown):
-        self.settings.set_string(self.key, self.items.backend_items[dropdown.get_selected()])
+    def select_item(self):
+        try:
+            if self.error:
+                self.set_error(False)
+
+            new_id = self.items.backend_items.index(self.settings.get_string(self.key))
+        except ValueError:
+            self.set_error(True)
+            new_list = self.errorless_model
+            new_list.append((self.settings.get_string(self.key)))
+            self.dropdown.set_model(new_list)
+
+            new_id = new_list.get_n_items() - 1
+        if new_id != self.dropdown.get_selected():
+            self.dropdown.set_selected(new_id)
+
+    def on_dropdown_changed(self, dropdown, selected_item):
+        try:
+            value = self.items.backend_items[self.dropdown.get_selected()]
+        except IndexError:
+            value = self.dropdown.get_model().get_string(self.dropdown.get_selected())
+        if value != self.settings.get_string(self.key):
+            self.settings.set_string(self.key, value)
 
 
-class DetailedDropdownRow(Adw.ExpanderRow, SettingObject):
+class DetailedDropdownRow(SettingObject, Adw.ExpanderRow):
     def __init__(self, title: str, schema: str, key: str, items: DropdownItems):
-        Adw.ExpanderRow.__init__(self, title)
         SettingObject.__init__(self, schema, key)
+        Adw.ExpanderRow.__init__(self)
+        self.set_title(title)
         self.items = items
         self.item_widgets = []
         self.radio_group = None
@@ -116,18 +171,144 @@ class DetailedDropdownRow(Adw.ExpanderRow, SettingObject):
         for item in self.item_widgets:
             if item.radio.get_active():
                 return item
+        return None
+
+    def item_clicked(self, item):
+        self.settings.set_string(self.key, item.backend_item)
+        self.set_subtitle(item.get_title())
 
     def on_setting_changed(self, *args):
-        pass
+        setting_value = self.settings.get_string(self.key)
+        for item in self.item_widgets:
+            if item.backend_item == setting_value:
+                item.radio.set_active(True)
+                self.set_subtitle(item.get_title())
+                self.remove_css_class("error")
+                return
+        self.add_css_class("error")
+
+    def check_error(self):
+        if self.get_selected() is None:
+            self.add_css_class("error")
+        else:
+            self.remove_css_class("error")
+
+    @classmethod
+    def new_from_items(cls, title: str, schema: str, key: str, items: DropdownItems):
+        dropdown_row = cls(title, schema, key, items)
+        for i in range(items.amount):
+            last_item = None
+            if i != 0:
+                last_item = dropdown_row.item_widgets[i - 1]
+            if items.item_descriptions is None:
+                raise ValueError("DetailedDropdownRow Items must have descriptions!")
+            item = DetailedDropdownItem(items.user_items[i], items.item_descriptions[i], items.backend_items[i],
+                                        dropdown_row, last_item)
+            dropdown_row.add_item(item)
+        dropdown_row.check_error()
+        return dropdown_row
 
 
 class DetailedDropdownItem(Adw.ActionRow):
     def __init__(self, title, subtitle, backend_item, dropdown_row, last_widget):
-        super().__init__(self, title=title, subtitle=subtitle)
+        Adw.ActionRow.__init__(self, title=title, subtitle=subtitle)
         self.backend_item = backend_item
         self.dropdown_row = dropdown_row
 
         self.radio = Gtk.CheckButton()
+        self.radio.set_halign(Gtk.Align.END)
         if last_widget is not None:
-            self.set_group(last_widget.radio)
+            self.radio.set_group(last_widget.radio)
+        if dropdown_row.settings.get_string(dropdown_row.key) == self.backend_item:
+            self.radio.set_active(True)
+            dropdown_row.set_subtitle(title)
         self.add_suffix(self.radio)
+        self.set_activatable(True)
+        self.set_activatable_widget(self.radio)
+
+        self.connect("activated", self.on_clicked)
+
+    def on_clicked(self, widget):
+        self.dropdown_row.item_clicked(self)
+
+
+class SpinButtonRow(ActionRow):
+    def __init__(self, title: str, subtitle: str | None, schema: str, key: str, value_type: str, minimum: int,
+                 maximum: int, step: int, percent: bool):
+        super().__init__(title, subtitle, schema, key)
+        self.percent = percent
+        if percent:
+            self.spin_button = Gtk.SpinButton.new_with_range(minimum * 100, maximum * 100, step * 100)
+        else:
+            self.spin_button = Gtk.SpinButton.new_with_range(minimum, maximum, step)
+        self.add_suffix(self.spin_button)
+
+        self.value_type = value_type
+        # Gets the value type because it could be any of these 3 values
+        if self.value_type == "int":
+            self.value = self.setting.get_int(key)
+        elif self.value_type == "uint":
+            self.value = self.setting.get_uint(key)
+        elif self.value_type == "double":
+            self.value = self.setting.get_double(key)
+        if self.percent is True:
+            self.value *= 100
+        self.spin_button.set_value(self.value)
+
+        self.spin_button.connect("value-changed", self.value_changed)
+        self.set_main_widget(self.spin_button)
+
+    def value_changed(self, spin_button):
+        self.value = self.spin_button.get_value()
+        if self.percent is True:
+            self.value /= 100
+
+        if self.value_type == "int":
+            self.setting.set_int(self.key, self.value)
+        elif self.value_type == "uint":
+            self.setting.set_uint(self.key, self.value)
+        elif self.value_type == "double":
+            self.setting.set_double(self.key, self.value)
+
+    def on_setting_changed(self, *args):
+        if self.setting.get_double(self.key) != self.spin_button.get_value():
+            new_value = self.setting.get_double(self.key)
+            if self.percent:
+                new_value *= 100
+            self.spin_button.set_value(new_value)
+
+
+# TODO: Implement this on Fedora 38
+# class FontRow(ActionRow):
+#     def __init__(self, title: str, subtitle: str, schema: str, key: str):
+#         super().__init__(title, subtitle, schema, key)
+#         self.font_dialog = Gtk.FontChooserDialog()
+#         self.font_dialog.set_title(title)
+#
+#
+#         self.font_button = Gtk.FontDialogButton()
+#         self.font_button.set_use_font(True)
+#         self.font_button.set_use_size(True)
+#
+#     def set_font(self, font):
+#         map = Pango.FontMap.
+#         self.font_dialog.set_font_map(
+#             Pango.FontMap.get_family(font)
+#         )
+#         Pango.FontMap.load_font(font)
+
+
+class rThemeAccentButton(Gtk.Button):
+    def __init__(self):
+        Gtk.Button.__init__(self)
+        self.set_halign(Gtk.Align.CENTER)
+        self.set_valign(Gtk.Align.CENTER)
+        self.set_size_request(30, 30)
+
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_data(ACCENT_BUTTON_CSS)
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
+            css_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
---
+++ main.py
@@ -0,0 +1,350 @@
+import gi
+import gettext
+import rthemelib
+import RtW
+import RtU
+
+_ = gettext.gettext
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+
+class Application(Adw.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.window = TweaksWindow()
+
+    def do_activate(self):
+        self.window.set_application(self)
+        self.window.present()
+
+
+class TweaksWindow(Adw.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_title(_("risiTweaks"))
+        self.set_default_size(800, 600)
+        self.set_icon_name("io.risi.Tweaks")
+
+        self.window_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_content(self.window_box)
+
+        self.header = Adw.HeaderBar()
+        self.window_box.append(self.header)
+
+        self.main_stack = Adw.ViewStack()
+        self.window_box.append(self.main_stack)
+
+        self.main_stack_switcher = Adw.ViewSwitcherTitle()
+        self.main_stack_switcher.set_stack(self.main_stack)
+        self.main_stack_switcher.set_title(_("risiTweaks"))
+        self.header.set_title_widget(self.main_stack_switcher)
+
+        # Appearance Page
+        self.appearance_page = Adw.PreferencesPage()
+        self.main_stack.add_titled_with_icon(self.appearance_page, "appearance_page", _("Appearance"), "preferences-desktop-wallpaper-symbolic")
+
+        # rTheme Group
+        self.rtheme_group = Adw.PreferencesGroup()
+        self.rtheme_group.set_title(_("rTheme"))
+        self.appearance_page.add(self.rtheme_group)
+        self.rtheme_group.add(
+            RtW.DropdownRow(
+                _("rTheme"),
+                _("The current color scheme used by rTheme."),
+                "io.risi.rtheme",
+                "theme-name",
+                RtW.DropdownItems.new_same_items(rthemelib.get_theme_list()),
+            )
+        )
+        self.rtheme_group.add(
+            RtW.DropdownRow(
+                _("rTheme Variant"),
+                _("Variant of the color scheme."),
+                "io.risi.rtheme", "variant-name",
+                RtW.DropdownItems.new_same_items([v.name for v in rthemelib.get_current_theme().variants]),
+            )
+        )
+        self.rtheme_group.add(
+            RtW.rThemeAccentButton()
+        )
+        self.rtheme_group.add(
+            RtW.DropdownRow(
+                _("Theme Styling"),
+                _("This is the same as the light and dark theme options in GNOME Control Center."),
+                "org.gnome.desktop.interface", "color-scheme",
+                RtW.DropdownItems(
+                    ["Default", "Dark"],
+                    ["default", "prefer-dark"],
+                ),
+            )
+        )
+
+        # Other Theming Group
+        self.other_theming_group = Adw.PreferencesGroup()
+        self.other_theming_group.set_title(_("Other Theming"))
+        self.appearance_page.add(self.other_theming_group)
+        self.other_theming_group.add(
+            RtW.DropdownRow(
+                _("Legacy GTK3/GTK4 Theme"),
+                _("This is the theme used by legacy applications that don't use Libadwaita.\n\nWARNING: "
+                "Changing this away from adw-gtk3/adw-gtk3-dark will remove rTheme support for non-Libadwaita apps."),
+                "org.gnome.desktop.interface", "gtk-theme",
+                RtW.DropdownItems.new_same_items(
+                    RtU.get_gtk_themes()
+                )
+            )
+        )
+        self.other_theming_group.add(
+            RtW.DropdownRow(
+                _("Icon Theme"),
+                _("This is the icon theme used by your system."),
+                "org.gnome.desktop.interface", "icon-theme",
+                RtW.DropdownItems.new_same_items(
+                    RtU.get_icon_themes(),
+                )
+            )
+        )
+        self.other_theming_group.add(
+            RtW.DropdownRow(
+                _("Cursor Theme"),
+                _("This is the theme used for your mouse pointer."),
+                "org.gnome.desktop.interface", "cursor-theme",
+                RtW.DropdownItems.new_same_items(
+                    RtU.get_cursor_themes()
+                )
+            )
+        )
+
+        # Appearance Page
+        self.layout_page = Adw.PreferencesPage()
+        self.main_stack.add_titled_with_icon(self.layout_page, "layout_page", _("Layout"),
+                                                "preferences-desktop-display-symbolic")
+
+        # Layout Group
+        self.layout_group = Adw.PreferencesGroup()
+        self.layout_group.set_title(_("Top Bar"))
+        self.layout_page.add(self.layout_group)
+        self.layout_group.add(
+            RtW.SwitchRow(
+                _("Allow Audio Volume Above 100%"),
+                _("This allows you to go above 100% volume in the top bar.\n\nWARNING: this may have an impact on audio "
+                "quality, and potentially could damage your audio hardware."),
+                "org.gnome.desktop.sound", "allow-volume-above-100-percent",
+            )
+        )
+        self.layout_group.add(
+            RtW.SwitchRow(
+                _("Show Battery Percentage"),
+                _("This shows the battery percentage in the top bar. This is only useful if you have a laptop."),
+                "org.gnome.desktop.interface", "show-battery-percentage",
+            )
+        )
+
+        # Layout Group
+        self.clock_group = Adw.PreferencesGroup()
+        self.clock_group.set_title(_("Top Bar Clock"))
+        self.layout_page.add(self.clock_group)
+        self.clock_group.add(
+            RtW.SwitchRow(
+                _("Show Weekday on Clock"),
+                _("Shows the day of the week on the top bar next to the clock."),
+                "org.gnome.desktop.interface", "clock-show-weekday",
+            )
+        )
+        self.clock_group.add(
+            RtW.SwitchRow(
+                _("Show Date on Clock"),
+                _("Show the date on the top bar."),
+                "org.gnome.desktop.interface", "clock-show-date",
+            )
+        )
+        self.clock_group.add(
+            RtW.SwitchRow(
+                _("Show Seconds on Clock"),
+                _("Show seconds in the top bar's clock."),
+                "org.gnome.desktop.interface", "clock-show-seconds",
+            )
+        )
+        self.clock_group.add(
+            RtW.SwitchRow(
+                _("Show Week Numbers on Calendar"),
+                _("Show week numbers in the calendar when you click on the top bar's clock."),
+                "org.gnome.desktop.interface", "show-weekdate",
+            )
+        )
+
+        # Window Group
+        self.window_group = Adw.PreferencesGroup()
+        self.window_group.set_title(_("Windows"))
+        self.layout_page.add(self.window_group)
+        self.window_group.add(
+            RtW.SwitchRow(
+                _("Attach Modal Dialogs"),
+                _("Decides whether dialogs that belong to the window can be moved separately to the parent window."),
+                "org.gnome.mutter", "attach-modal-dialogs",
+            )
+        )
+        self.window_group.add(
+            RtW.SwitchRow(
+                _("Center New Windows"),
+                _("Whether or not to start new windows in the center of the screen."),
+                "org.gnome.mutter", "center-new-windows",
+            )
+        )
+        self.window_group.add(
+            RtW.DropdownRow(
+                _("Window Action Key"),
+                _("Holding this key basically treats the whole window like the titlebar and allows moving it and "
+                  "right clicking it for options."),
+                "org.gnome.desktop.wm.preferences", "mouse-button-modifier",
+                RtW.DropdownItems(
+                    ["None", "Alt", "Super (Default)"],
+                    ["none", "<Alt>", "<Super>"],
+                )
+            )
+        )
+        self.window_group.add(
+            RtW.DropdownRow(
+                _("Titlebar Button Layout"),
+                _("This is the layout of the titlebar buttons."),
+                "org.gnome.desktop.wm.preferences", "button-layout",
+                RtW.DropdownItems(
+                    [
+                        "risiOS Left", "risiOS Right (Default)", "elementary", "elementary inverted",
+                        "elementary with minimize", "elementary with minimize inverted", "GNOME Left", "GNOME Right",
+                        "macOS Left", "macOS Right", "Windows Left", "Windows Right"
+                    ],
+                    [
+                        "minimize,close:appmenu", "appmenu:minimize,close", "close,appmenu:maximize",
+                        "maximize,appmenu:close", "close,appmenu:minimize", "minimize,appmenu:close",
+                        "close:appmenu", "appmenu:close", "close,minimize,maximize:appmenu",
+                        "appmenu:maximize,minimize,close", "close,maximize,minimize:appmenu",
+                        "appmenu:minimize,maximize,close",
+                    ],
+                )
+            )
+        )
+
+        # Window Titlebar Action Group
+        self.window_titlebar_action_group = Adw.PreferencesGroup()
+        self.window_titlebar_action_group.set_title(_("Titlebar Actions"))
+        self.window_titlebar_action_group.set_description(_(
+            "These are the actions that happen when you click on the titlebar."
+        ))
+        self.layout_page.add(self.window_titlebar_action_group)
+        self.window_titlebar_action_group.add(
+            RtW.DropdownRow(
+                _("Double Click Action"), None, "org.gnome.desktop.wm.preferences", "action-double-click-titlebar",
+                RtW.DropdownItems(
+                    ["Lower Window", "Open Menu", "Toggle Maximize", "Minimize", "Nothing"],
+                    ["lower", "menu", "toggle-maximize", "minimize", "none"]
+                )
+            )
+        )
+        self.window_titlebar_action_group.add(
+            RtW.DropdownRow(
+                _("Middle Click Action"), None, "org.gnome.desktop.wm.preferences", "action-middle-click-titlebar",
+                RtW.DropdownItems(
+                    ["Lower Window", "Open Menu", "Toggle Maximize", "Minimize", "Nothing"],
+                    ["lower", "menu", "toggle-maximize", "minimize", "none"]
+                )
+            )
+        )
+        self.window_titlebar_action_group.add(
+            RtW.DropdownRow(
+                _("Right Click Action"), None, "org.gnome.desktop.wm.preferences", "action-right-click-titlebar",
+                RtW.DropdownItems(
+                    ["Lower Window", "Open Menu", "Toggle Maximize", "Minimize", "Nothing"],
+                    ["lower", "menu", "toggle-maximize", "minimize", "none"]
+                )
+            )
+        )
+
+        # Keyboard and Mouse Page
+        self.keyboard_mouse_page = Adw.PreferencesPage()
+        self.main_stack.add_titled_with_icon(self.keyboard_mouse_page, "keyboard_mouse_page", _("Keyboard & Mouse"), "input-keyboard-symbolic")
+
+        # Keyboard Group
+        self.keyboard_group = Adw.PreferencesGroup()
+        self.keyboard_group.set_title(_("Keyboard"))
+        self.keyboard_mouse_page.add(self.keyboard_group)
+        self.keyboard_group.add(
+            RtW.SwitchRow(
+                _("Show Extended Input Sources in Settings"),
+                _("Show extended input sources in GNOME Control Center's Keyboard settings."),
+                "org.gnome.desktop.input-sources", "show-all-sources",
+            )
+        )
+        self.keyboard_group.add(
+            RtW.DropdownRow(
+                _("Keybinding Preset (GTK Apps Only)"),
+                _("This modifies some keybindings in GTK apps."),
+                "org.gnome.desktop.interface", "gtk-key-theme",
+                RtW.DropdownItems.new_lowercase_items(
+                    ["Default", "Emacs"],
+                )
+            )
+        )
+
+        # Mouse Group
+        self.mouse_group = Adw.PreferencesGroup()
+        self.mouse_group.set_title(_("Mouse"))
+        self.keyboard_mouse_page.add(self.mouse_group)
+        self.mouse_group.add(
+            RtW.DetailedDropdownRow.new_from_items(
+                _("Acceleration Profile"),
+                "org.gnome.desktop.peripherals.mouse", "accel-profile",
+                RtW.DropdownItems.new_lowercase_items(
+                    [_("Flat"), _("Adaptive"), _("Default")],
+                    [
+                        _("No mouse acceleration. Less precise for short distances but better for things like FPS "
+                          "games."),
+                        _("Adaptive mouse acceleration. Adapts the acceleration based on the movement speed."),
+                        _("Default mouse acceleration (Recommended). Let's the mouse decide it's own acceleration."),
+                    ]
+                )
+            )
+        )
+        self.mouse_group.add(
+            RtW.SwitchRow(
+                _("Middle Click Paste"),
+                _("Whether or not to paste when you middle click."),
+                "org.gnome.desktop.interface", "gtk-enable-primary-paste",
+            )
+        )
+
+        # Mouse Group
+        self.touchpad_group = Adw.PreferencesGroup()
+        self.touchpad_group.set_title(_("Touchpad"))
+        self.keyboard_mouse_page.add(self.touchpad_group)
+        self.touchpad_group.add(
+            RtW.DetailedDropdownRow.new_from_items(
+                _("Mouse Click Emulation"),
+                "org.gnome.desktop.peripherals.touchpad", "click-method",
+                RtW.DropdownItems(
+                    [_("Fingers"), _("Areas"), _("Disabled")],
+                    ["fingers", "areas", "none"],
+                    [
+                        _("Click the touchpad with two fingers for right-click, and three fingers for middle click."),
+                        _("Click the bottom right of the touchpad for right click and the bottom middle for middle click."),
+                        _("Don't use mouse click emulation."),
+                    ]
+                )
+            )
+        )
+        self.touchpad_group.add(
+            RtW.SwitchRow(
+                _("Disable Touchpad While Typing"),
+                _("Prevents you from accidentally moving the touchpad while typing."),
+                "org.gnome.desktop.peripherals", "disable-while-typing",
+            )
+        )
+
+
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(None)
